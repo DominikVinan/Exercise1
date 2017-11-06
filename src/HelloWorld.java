758cf78bddc0623c3cad1f340cf4cd34b177947e
Dominik Vinan <dominik.vinan@tum.de>
2017-11-06 09:59:37 +0100

    Add HelloUser class. Modify HelloWorld class to read a name and greet the user.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		Scanner input = new Scanner(System.in);
+		String userName = input.nextLine();
+		
+		HelloUser helloUser = new HelloUser(userName);
+		helloUser.greetUser();
 	}
 
 }
